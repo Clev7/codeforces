c9b0f8399ad338624aa100f2ba8d650090d609f2
Yousef Abdelsalam <yousefeid707@gmail.com>
2025-01-10 00:21:36 -0800

    281a

---
+++ 281a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<long long>;
+
+int main(void) {
+    string s;
+
+    getline(cin, s);
+
+    s[0] = toupper(s[0]);
+
+    cout << s << '\n';
+}
